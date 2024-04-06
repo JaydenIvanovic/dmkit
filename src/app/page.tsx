649de7d4177d7dd85b6b8ffb25c9232ea2aab87b
Jayden Ivanovic <jayden.ivanovic@gmail.com>
2024-04-06 11:05:20 +1100

    Add box shadow to main image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center w-full">
       <Image
-        className="[border-radius:100%]"
+        className="[border-radius:100%] [box-shadow:0px_0px_20px_15px_#37243a]"
         src="/images/dungeon-master.jpg"
         alt="A wizard in a dungeon rolling d20 dice"
         width={500}
